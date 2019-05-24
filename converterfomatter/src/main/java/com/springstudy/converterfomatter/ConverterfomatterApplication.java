090f0b5fdda9dd2e3c5bb5c4a4117a79bb9f2198
Seongmun-Hong <hsm63746244@gmail.com>
2019-05-24 13:27:57 +0900

    add :  - Converter, Formatter 추가

---
+++ converterfomatter/src/main/java/com/springstudy/converterfomatter/ConverterfomatterApplication.java
@@ -0,0 +1,13 @@
+package com.springstudy.converterfomatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverterfomatterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConverterfomatterApplication.class, args);
+    }
+
+}
---
+++ converterfomatter/src/test/java/com/springstudy/converterfomatter/ConverterfomatterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springstudy.converterfomatter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConverterfomatterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
